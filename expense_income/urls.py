1219e9380beef9dc128600ed145c2c5195165f20
Paul Diaz <paulcardozo_01@outlook.com>
2025-08-25 23:23:32 -0500

    feat(category): update categories section design
    - Redesign categories section user interface
    - Implement category deletion functionality
    - Integrate Google Fonts to improve typography
    - Enhance overall user experience

--- expense_income/urls.py
+++ expense_income/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, category, budget, create_category,edit_category
+from .views import home, category, budget, create_category,edit_category,remove_category
 
 app_name = 'expense_income'
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('budget/', budget, name='budget'),
     path('category/', category, name='category'),
     path('category/create', create_category, name="create_category"),
-    path('category/edit/<int:id_category>', edit_category, name="edit_category")
+    path('category/edit/<int:id_category>', edit_category, name="edit_category"),
+    path('category/remove/<int:id_category>',remove_category,name="remove_category")
 ]
--- expense_income/views.py
+++ expense_income/views.py
@@ -13,13 +13,25 @@ def home(request):
 def category(request):
     """Category Section"""
     choices_category = Category.TypeCategory.choices
-    categories_expense = Category.objects.filter(
-        user=request.user, type_category=Category.TypeCategory.EXPENSE).order_by('-id_category')
-    categories_income = Category.objects.filter(
-        user=request.user, type_category=Category.TypeCategory.INCOME).order_by('-id_category')
+    order_income = request.GET.get("order_income", "asc")
+    order_expense = request.GET.get("order_expense", "asc")
+    list_categories_expenses = Category.objects.filter(
+        user=request.user, type_category=Category.TypeCategory.EXPENSE)
+    list_categories_incomes = Category.objects.filter(
+        user=request.user, type_category=Category.TypeCategory.INCOME)
+
+    categories_expense = list_categories_expenses.order_by(
+        'name_category' if order_expense == "asc" else "-name_category")
+    categories_income = list_categories_incomes.order_by(
+        'name_category' if order_income == "asc" else "-name_category")
+    order_income = "asc" if order_income == "asc" else "desc"
+    order_expense = "asc" if order_expense == "asc" else "desc"
+
     context = {'choices_category': choices_category, 'user': request.user,
                'categories_income': categories_income,
-               'categories_expense': categories_expense}
+               'categories_expense': categories_expense,
+               'order_income': order_income, 'order_expense': order_expense
+               }
 
     return render(request, 'expense_income/category.html', context)
 
@@ -36,18 +48,26 @@ def create_category(request):
 
 
 @login_required(login_url='/login')
-def edit_category(request,id_category):
+def edit_category(request, id_category):
     """Edit Category"""
     if request.method == 'POST':
         name = request.POST['category_name']
         category_type = request.POST['category_type']
-        category = Category.objects.get(user = request.user, id_category = id_category)
+        category = Category.objects.get(
+            user=request.user, id_category=id_category)
         category.name_category = name
         category.type_category = category_type
         category.save()
 
         return redirect('expense_income:category')
-    
+
+
+@login_required(login_url='/login')
+def remove_category(request, id_category):
+    category = Category.objects.get(id_category=id_category)
+    category.delete()
+    return redirect('expense_income:category')
+
 
 @login_required(login_url='/login')
 def budget(request):
