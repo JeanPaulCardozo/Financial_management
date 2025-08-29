50a57899376aee1e6d4b0c34b7d342efdde05e5c
Paul Diaz <paulcardozo_01@outlook.com>
2025-08-28 21:45:08 -0500

    chore: Add decorators
    - Add decorator capitalize in the transaction section
    
    - add tag to greet the user

---
+++ expense_income/templatetags/greet.py
@@ -0,0 +1,16 @@
+"""tag to greet"""
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+@register.filter
+def greet(value=None):
+    """get Greet by hour"""
+    hora = datetime.now().hour
+    if 6 <= hora < 12:
+        return "Buenos días"
+    elif 12 <= hora < 18:
+        return "Buenas tardes"
+    else:
+        return "Buenas noches"
